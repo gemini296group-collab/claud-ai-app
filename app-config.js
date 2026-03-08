fbfcb37a6d1169b6e354a0d62f727b3ed3fe367d
296 GROUP <gemini296group@gmail.com>
2026-03-08 13:08:32 +0500

    add supabase keys

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
-window.__APP_CONFIG__ = window.__APP_CONFIG__ || {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+window.__APP_CONFIG__ = {
+  SUPABASE_URL: "https://hhmwxxksbgcnlphoepbr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_Yr1DdtDx0ULiK3wJhwMJvw_p0cSK6b5"
 };
